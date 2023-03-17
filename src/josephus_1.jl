3a7f19a5975c19e12d1a33a12cfdf2f05d577b73
TheKonamiKoder <thekonamicode02@gmail.com>
2023-03-17 19:30:32 +0000

    Created josephus_0, josephus_1 functions.

--- src/josephus_1.jl
+++ src/josephus_1.jl
@@ -1,11 +1,12 @@
 
-function josephus_0(n::Integer)::Integer
+function josephus_1(n::Integer, k::Integer)::Integer
     next = 0
     circle = [1:n...]
     while length(circle) > 1
-        next = (next + 1) % length(circle)
+        next = (next + k - 1) % length(circle)
         deleteat!(circle, next + 1)
     end
 
     circle[1]
 end
+
