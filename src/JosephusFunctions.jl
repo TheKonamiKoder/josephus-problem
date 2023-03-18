9d10bf9f9e0be18df03551696a46701fbedc660b
TheKonamiKoder <thekonamicode02@gmail.com>
2023-03-18 20:12:26 +0000

    Added josephus_2 to the module

--- src/JosephusFunctions.jl
+++ src/JosephusFunctions.jl
@@ -1,6 +1,7 @@
 module JosephusFunctions
-    export josephus_0, josephus_1
+    export josephus_0, josephus_1, josephus_2
     
     include("josephus_0.jl")
     include("josephus_1.jl")
+    include("josephus_2.jl")
 end
