9cc869de0fbb1be2db1fad2834d4d828bb82f9a3
TheKonamiKoder <thekonamicode02@gmail.com>
2023-03-18 20:14:25 +0000

    Fixed bug with namedtuplse

--- src/josephus_2.jl
+++ src/josephus_2.jl
@@ -5,7 +5,7 @@ function josephus_2(n::Integer, k::Integer, l::Integer)::Integer
 
     while length(circle) > 0
         next = (next + k - 1) % length(circle)
-        circle[next + 1].lives -= 1
+        circle[next + 1] = (position=circle[next + 1].position, lives=circle[next + 1].lives - 1)
         if circle[next + 1].lives == 0
             deleteat!(circle, next + 1)
         end
