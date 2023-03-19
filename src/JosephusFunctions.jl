7b6cab9f00699bb599cbec4454e794be2ebebeb3
TheKonamiKoder <thekonamicode02@gmail.com>
2023-03-19 11:09:51 +0000

    Added josephus_3 using functions to determine next

--- src/JosephusFunctions.jl
+++ src/JosephusFunctions.jl
@@ -1,7 +1,51 @@
 module JosephusFunctions
-    export josephus_0, josephus_1, josephus_2
+    export josephus_0, josephus_1, josephus_2, josephus_3
     
-    include("josephus_0.jl")
-    include("josephus_1.jl")
-    include("josephus_2.jl")
+    function josephus_0(n::Integer)::Integer
+        next = 0
+        circle = [1:n...]
+        while length(circle) > 1
+            next = (next + 1) % length(circle)
+            deleteat!(circle, next + 1)
+        end
+    
+        circle[1]
+    end
+    
+    function josephus_1(n::Integer, k::Integer)::Integer
+        next = 0
+        circle = [1:n...]
+        while length(circle) > 1
+            next = (next + k - 1) % length(circle)
+            deleteat!(circle, next + 1)
+        end
+    
+        circle[1]
+    end
+    
+    function josephus_2(n::Integer, k::Integer, l::Integer)::Integer
+        next = 0
+        circle = [(position=position, lives=l) for position in 1:n]
+    
+        while length(circle) > 1
+            next = (next + k - 1) % length(circle)
+            circle[next + 1] = (position=circle[next + 1].position, lives=circle[next + 1].lives - 1)
+            if circle[next + 1].lives == 0
+                deleteat!(circle, next + 1)
+            end
+        end
+    
+        circle[1].position
+    end
+
+    function josephus_3(n::Integer, f::Function)::Integer
+        next = 0
+        circle = [1:n...]
+        while length(circle) > 1
+            next = f(next) % length(circle)
+            deleteat!(circle, next + 1)
+        end
+
+        circle[1]
+    end
 end
